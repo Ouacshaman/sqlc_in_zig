70e2b4e520427ec4a2c89929cdb2e7f497bb6824
Shi Hong <shaquanhong@gmail.com>
2025-03-03 01:01:12 -0500

    adjusted the createFn function in sql2zig*.zig file

--- src/sql2fn_handler.zig
+++ src/sql2fn_handler.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
 
-pub fn createFn(name: []const u8, query: []const u8, alloc: std.mem.Allocator) ![]const u8 {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
+pub fn createFn(name: []const u8, query: []const u8, allocator: std.mem.Allocator) ![]const u8 {
     var list = std.ArrayList(u8).init(allocator);
     defer list.deinit();
 
@@ -12,16 +8,10 @@ pub fn createFn(name: []const u8, query: []const u8, alloc: std.mem.Allocator) !
     const end_of_name = std.mem.indexOf(u8, name[11..], ":");
 
     const signature = try std.fmt.allocPrint(allocator, "\npub fn {s}(stream: std.net.Stream, allocator: std.mem.Allocator) !void{s}\n", .{ name[11 .. 11 + end_of_name.? - 1], "{" });
-    defer allocator.free(signature);
     try list.appendSlice(signature);
 
     const query_line = try std.fmt.allocPrint(allocator, "    try query.sendQuery(stream, allocator, \"{s}\");\n{s}\n", .{ query, "}" });
-    defer allocator.free(query_line);
     try list.appendSlice(query_line);
 
-    const joined = try list.toOwnedSlice();
-    defer allocator.free(joined);
-    const res = try alloc.dupe(u8, joined);
-
-    return res;
+    return try list.toOwnedSlice();
 }
